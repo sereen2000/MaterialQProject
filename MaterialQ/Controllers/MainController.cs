138d351189c657d80522c43adb0124eca6bcad0b
sereen2000 <127016744+sereen2000@users.noreply.github.com>
2025-11-06 13:52:51 +0300

    MaterialQ Project

---
+++ MaterialQ/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaterialQ.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+      
+    }
+}
---
+++ MaterialQ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MaterialQ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
